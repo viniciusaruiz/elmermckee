23e8c276a4312d09689718bd2039f6c11edb6e01
vivsoliveira <victoriaof.al.insper.edu.br>
2022-11-11 13:54:28 -0300

    base de questoes

---
+++ base_questoes.py
@@ -0,0 +1,268 @@
+quest = [{'titulo': 'Qual o resultado da operação 57 + 32?',
+          'nivel': 'facil',
+          'opcoes': {'A': '-19', 'B': '85', 'C': '89', 'D': '99'},
+          'correta': 'C'},
+
+         {'titulo': 'Qual a capital do Brasil?',
+          'nivel': 'facil',
+          'opcoes': {'A': 'Brasília', 'B': 'Rio de janeiro', 'C': 'São Paulo', 'D': 'Osasco'},
+          'correta': 'A'},
+
+         {'titulo': 'Quando é o feriado da Independência do Brasil?',
+          'nivel': 'facil',
+          'opcoes': {'A': '21 de Abril', 'B': '12 de Outubro', 'C': '07 de Setembro', 'D': '15 de Novembro'},
+          'correta': 'C'},
+
+         {'titulo': '_________ é um conjunto de particularidades que caracterizam um grupo de pessoas, uma família ou uma sociedade. É formada por princípios morais, hábitos, costumes, histórias, manifestações religiosas, entre outros. Qual palavra melhor completa o início da frase?',
+          'nivel': 'facil',
+          'opcoes': {'A': 'Missão', 'B': 'Cultura', 'C': 'Curso superior', 'D': 'Culinária'},
+          'correta': 'B'},
+
+         {'titulo': 'Qual destes termos menos tem relação com o fenômeno da globalização?',
+          'nivel': 'facil',
+          'opcoes': {'A': 'Aculturação', 'B': 'Neoliberalismo', 'C': 'União Europeia', 'D': 'Caldeirão do Huck'},
+          'correta': 'D'},
+
+         {'titulo': 'Qual o feriado do aniversário da cidade de São Paulo?',
+          'nivel': 'facil',
+          'opcoes': {'A': '25 de Janeiro', 'B': '24 de Março', 'C': '9 de Julho', 'D': '12 de Novembro'},
+          'correta': 'A'},
+
+         {'titulo': 'Qual destas não é uma fruta?',
+          'nivel': 'facil',
+          'opcoes': {'A': 'Laranja', 'B': 'Maça', 'C': 'Tomate', 'D': 'Abacate'},
+          'correta': 'B'},
+
+         {'titulo': 'Em qual ano o TikTok atingiu 1 bilhão de usuários?',
+          'nivel': 'facil',
+          'opcoes': {'A': '2019', 'B': '2021', 'C': '2015', 'D': '2018'},
+          'correta': 'B'},
+         
+         {'titulo': 'Qual destes não é um app com foco em streaming de vídeo?',
+          'nivel': 'facil',
+          'opcoes': {'A': 'Netflix', 'B': 'Disney Plus', 'C': 'TIDAL', 'D': 'HBO Max'},
+          'correta': 'C'},
+
+         {'titulo': 'Qual destes parques não se localiza em São Paulo?!',
+          'nivel': 'facil',
+          'opcoes': {'A': 'Ibirapuera', 'B': 'Parque do Carmo', 'C': 'Parque Villa Lobos', 'D': 'Morro da Urca'},
+          'correta': 'D'},
+
+         {'titulo': 'Qual destas não é uma linguagem de programação?',
+          'nivel': 'facil',
+          'opcoes': {'A': 'Miratdes', 'B': 'Python', 'C': 'Lua', 'D': 'C++'},
+          'correta': 'A'},
+
+         {'titulo': 'Dentre os listados, qual destes esportes é menos praticado no Brasil?',
+          'nivel': 'facil',
+          'opcoes': {'A': 'Natação', 'B': 'Vôlei', 'C': 'Ski Cross Country', 'D': 'Futebol'},
+          'correta': 'C'},
+         
+         {'titulo': 'Qual o resultado da operação 5 + 2 * 3?',
+          'nivel': 'medio',
+          'opcoes': {'A': '21', 'B': '11', 'C': '30', 'D': '10'},
+          'correta': 'B'},
+
+         {'titulo': 'Qual destas é uma pseudociência que estuda os corpos celestes e as prováveis relações que possuem com a vida das pessoas e os acontecimentos na Terra?',
+          'nivel': 'medio',
+          'opcoes': {'A': 'Astronomia', 'B': 'Física quântica', 'C': 'Astrologia', 'D': 'Computação'},
+          'correta': 'C'},
+
+         {'titulo': 'Qual destas não foi considerada em 2007 uma das sete maravilhas do mundo moderno?',
+          'nivel': 'medio',
+          'opcoes': {'A': 'Muralha da China', 'B': 'Machu Picchu', 'C': 'Cristo Redentor', 'D': 'Torre Eiffel'},
+          'correta': 'D'},
+
+         {'titulo': 'Qual destas pessoas conduziu importantes estudos sobre radioatividade, sendo ganhadora de dois prêmios Nobel?',
+          'nivel': 'medio',
+          'opcoes': {'A': 'Marie Curie', 'B': 'Paul Erdős', 'C': 'Clive W.J. Granger', 'D': 'Maria Ressa'},
+          'correta': 'A'},
+
+         {'titulo': 'Quem é considerada a primeira pessoa programadora do mundo?!',
+          'nivel': 'medio',
+          'opcoes': {'A': 'Marie Curie', 'B': 'Alan Turing', 'C': 'Ada Lovelace', 'D': 'Edsger Dijkstra'},
+          'correta': 'C'},
+
+         {'titulo': 'Qual destes números é primo?',
+          'nivel': 'medio',
+          'opcoes': {'A': '259', 'B': '85', 'C': '49', 'D': '19'},
+          'correta': 'D'},
+
+         {'titulo': 'Na Conjectura de _______, escolhendo-se um número natural inicial n, onde n > 0, os seguintes critérios serão obedecidos: Se n for par o seu sucessor será a metade e se n for ímpar o seu sucessor será o triplo mais um, gerando então um novo número. Qual o nome da conjectura?',
+          'nivel': 'medio',
+          'opcoes': {'A': 'Collatz', 'B': 'Goldbach', 'C': 'Poincaré', 'D': 'Hodge'},
+          'correta': 'A'},
+
+         {'titulo': 'Como faço para chamar o SAMU?',
+          'nivel': 'medio',
+          'opcoes': {'A': 'Ligue 101', 'B': 'Ligue 192', 'C': 'Ligue 109', 'D': 'Ligue 122'},
+          'correta': 'B'},
+
+         {'titulo': 'Qual a segunda pessoa mais seguida no Instagram?',
+          'nivel': 'medio',
+          'opcoes': {'A': 'Cristiano Ronaldo', 'B': 'Dwayne Johnson', 'C': 'Kim Kardashian', 'D': 'Kylie Jenner'},
+          'correta': 'D'},
+
+         {'titulo': 'Qual a pessoa mais seguida no Instagram?',
+          'nivel': 'medio',
+          'opcoes': {'A': 'Cristiano Ronaldo', 'B': 'Dwayne Johnson', 'C': 'Kim Kardashian', 'D': 'Lionel Messi'},
+          'correta': 'A'},
+
+         {'titulo': 'A reprodução dos seres vivos é um processo biológico através do qual os organismos geram descendência. Qual desta não é uma forma de reprodução assexuada?',
+          'nivel': 'dificil',
+          'opcoes': {'A': 'Autogamia', 'B': 'Esporulação', 'C': 'Partenogênese', 'D': 'Divisão binária'},
+          'correta': 'A'},
+
+         {'titulo': 'Qual o resultado da operação 5 + 2 * 3 ^ 2, onde ^ representa potenciação?',
+          'nivel': 'dificil',
+          'opcoes': {'A': '441', 'B': '86', 'C': 'Nenhuma das outras respostas', 'D': '23'},
+          'correta': 'D'},
+
+         {'titulo': 'Quem é Oxóssi?!',
+          'nivel': 'dificil',
+          'opcoes': {'A': 'Rede de mercados', 'B': 'Tipo de poema Dissílabo', 'C': 'Divindade das religiões africanas', 'D': 'Trapper brasileiro'},
+          'correta': 'C'},
+
+         {'titulo': 'Qual a altura do Cristo Redentor?',
+          'nivel': 'dificil',
+          'opcoes': {'A': 'entre 0 e 20 metros', 'B': 'Entre 21 e 40 metros', 'C': 'Entre 41 e 60 metros', 'D': 'Mais que 60 metros'},
+          'correta': 'B'},
+
+         {'titulo': 'Em que ano faleceu Charles Babbage?',
+          'nivel': 'dificil',
+          'opcoes': {'A': '2022', 'B': '1791', 'C': '1935', 'D': '1871'},
+          'correta': 'A'},
+
+         {'titulo': 'Einstein foi Nobel de física em qual ano?',
+          'nivel': 'dificil',
+          'opcoes': {'A': '1906', 'B': '1905', 'C': '1920', 'D': '1921'},
+          'correta': 'D'},
+
+         {'titulo': 'Qual o número atômico do nitrogênio?',
+          'nivel': 'dificil',
+          'opcoes': {'A': '9', 'B': '7', 'C': '6', 'D': '8'},
+          'correta': 'B'},
+
+         {'titulo': 'Qual o ponto de fusão do nitrogênio?',
+          'nivel': 'dificil',
+          'opcoes': {'A': '120º C', 'B': '15º C', 'C': '-210º C', 'D': '-180º C'},
+          'correta': 'C'},
+         
+         {'titulo': 'Quantos gols Pelé fez oficialmente?',
+          'nivel': 'dificil',
+          'opcoes': {'A': '815', 'B': '762', 'C': '1100', 'D': '1057'},
+          'correta': 'B'},
+
+         {'titulo': 'O que é Necrose?',
+          'nivel': 'dificil',
+          'opcoes': {'A': 'Uma banda de Rock', 'B': 'Uma marca de luxo', 'C': 'Cidade Francesa', 'D': 'Morte de tecido orgânico'},
+          'correta': 'D'},
+          {'titulo': 'Qual destas ferramentas é mais apropriada para cavar pregos em uma superfície?',
+          'nivel': 'facil',
+          'opcoes': {'A': 'Alicate', 'B': 'Martelo', 'C': 'Serrote', 'D': 'Lixadeira'},
+          'correta': 'B'},
+
+         {'titulo': 'Na alimentação vegetariana, qual desses alimentos não é consumido?',
+          'nivel': 'facil',
+          'opcoes': {'A': 'Carne', 'B': 'Abobrinha', 'C': 'Lentilha', 'D': 'Ervilha'},
+          'correta': 'A'},
+
+         {'titulo': 'Um ano é composto de quantos trimestres',
+          'nivel': 'facil',
+          'opcoes': {'A': '2', 'B': '3', 'C': '4', 'D': '6'},
+          'correta': 'C'},
+
+         {'titulo': 'Qual é o filhote da ovelha?',
+          'nivel': 'facil',
+          'opcoes': {'A': 'ovelha filho', 'B': 'cabrito', 'C': 'cordeiro', 'D': 'bode'},
+          'correta': 'C'},
+
+         {'titulo': 'Quando é celebrado o dia internacional da mulher?',
+          'nivel': 'facil',
+          'opcoes': {'A': '8 de março', 'B': '10 de setembro', 'C': '12 de abril', 'D': '20 de julho'},
+          'correta': 'A'},
+
+         {'titulo': 'Qual dessas obras não é de Leonardo da Vinci',
+          'nivel': 'facil',
+          'opcoes': {'A': 'Mona Lisa', 'B': 'Davi', 'C': 'A Última Ceia', 'D': 'Homem Vitruviano'},
+          'correta': 'B'},
+          
+         {'titulo': 'Qual o volume ocupado por um gás na CNTP?',
+          'nivel': 'facil',
+          'opcoes': {'A': '21', 'B': '32L', 'C': '25L', 'D': '22,4L'},
+          'correta': 'D'},
+
+         {'titulo': 'Qual é o verdadeiro nome do Super-Homem?',
+          'nivel': 'facil',
+          'opcoes': {'A': 'Bruce Wayne', 'B': 'Clark Kent', 'C': 'Barry Allen', 'D': 'Oliver Queen'},
+          'correta': 'B'},
+
+         {'titulo': 'Qual é o livro mais vendido no mundo?',
+          'nivel': 'facil',
+          'opcoes': {'A': 'Bíblia', 'B': 'Harry Potter', 'C': 'Dom Quixote', 'D': 'O Pequeno Príncipe'},
+          'correta': 'A'},
+          
+         {'titulo': 'Complete o ditado: Galinha que canta...',
+          'nivel': 'facil',
+          'opcoes': {'A': 'canta sozinha', 'B': 'acorda os outros', 'C': 'da pintinho', 'D': 'bota o ovo'},
+          'correta': 'D'},
+
+         {'titulo': 'De quem é o álbum póstumo mais vendido de todos os tempos?',
+          'nivel': 'facil',
+          'opcoes': {'A': 'Avicci', 'B': 'Elvis Presley', 'C': 'Michael Jackson', 'D': 'Justin Bieber'},
+          'correta': 'C'},
+
+         {'titulo': 'Quantas pontas tem a estrela de Davi?',
+          'nivel': 'facil',
+          'opcoes': {'A': '7', 'B': '6', 'C': '8', 'D': '5'},
+          'correta': 'B'},
+
+         {'titulo': 'Se o pai to Padre é filho do meu pai, o que o Padre é meu?',
+          'nivel': 'médio',
+          'opcoes': {'A': 'Irmão', 'B': 'Tio', 'C': 'Primo', 'D': 'Sobrinho'},
+          'correta': 'D'},
+
+         {'titulo': 'No filme Aladdin, qual era o nome de seu macaco de estimação?',
+          'nivel': 'médio',
+          'opcoes': {'A': 'Mico', 'B': 'Babu', 'C': 'Rafiki', 'D': 'Abu'},
+          'correta': 'D'},
+
+         {'titulo': 'Quantas cores há no arco-írias?',
+          'nivel': 'médio',
+          'opcoes': {'A': '5', 'B': '6', 'C': '7', 'D': '8'},
+          'correta': 'C'},
+
+         {'titulo': 'Qual é o nome do estádio do Santos?',
+          'nivel': 'médio',
+          'opcoes': {'A': 'Vila Belmiro', 'B': 'Bruno Ramalho', 'C': 'Moisés Lucarelli', 'D': 'Brinco de Ouro da Princesa'},
+          'correta': 'A'},
+
+         {'titulo': 'Qual é o maior planeta do sistema solar?',
+          'nivel': 'médio',
+          'opcoes': {'A': 'Saturno', 'B': 'Júpiter', 'C': 'Marte', 'D': 'Plutão'},
+          'correta': 'B'},
+
+         {'titulo': 'Quais são as cinco cores dos anéis dos Jogos Olímpicos?',
+          'nivel': 'médio',
+          'opcoes': {'A': 'Azul, vermelho, amarelo, verde e preto', 'B': 'Azul, vermelho, amarelo, verde e violeta', 'C': 'Índigo, vermelho, roxo, verde e preto', 'D': 'Azul, vermelho, amarelo, laranja e verde'},
+          'correta': 'A'},
+
+         {'titulo': 'Qual classificação dada ao medicamento obtido exclusivamete da matéria-prima vegetal',
+          'nivel': 'médio',
+          'opcoes': {'A': 'Genérico', 'B': 'Fitoterápico', 'C': 'Homeopático', 'D': 'Alopático'},
+          'correta': 'B'},
+
+         {'titulo': 'Em qual destas regiões ficava o Quilombo dos Palmares, onde viveu Zumbi?',
+          'nivel': 'difícil',
+          'opcoes': {'A': 'Serra da Barriga, AL', 'B': 'Vale do Ribeira, SP', 'C': 'Riacho de Santana, BA', 'D': 'Boqueirão da Arara, CE'},
+          'correta': 'A'},
+
+         {'titulo': 'Quantos jogadores há em um time de beisebol?',
+          'nivel': 'difícil',
+          'opcoes': {'A': '10', 'B': '9', 'C': '8', 'D': '7'},
+          'correta': 'B'},  
+
+         {'titulo': 'Qual é o aumentativo de navio?',
+          'nivel': 'difícil',
+          'opcoes': {'A': 'Não existe', 'B': 'Navião', 'C': 'Grande navio', 'D': 'Naviarra'},
+          'correta': 'D'}]
